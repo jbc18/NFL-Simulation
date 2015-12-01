9a43dece3204b2c222fd5a58a798096ab615d904
jbc18 <mrjbc18@gmail.com>
2015-12-01 14:48:55 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
+//
+//  main.c
+//  asdf
+//
+//  Created by Jarrett Cathcart on 8/31/15.
+//  Copyright (c) 2015 Jarrett Cathcart. All rights reserved.
+//
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+struct stats {
+    int total_yards;
+    int total_passing_yards;
+    int total_running_yards;
+    int first_downs;
+};
+
+int total_yards;
+int total_passing_yards;
+int total_running_yards;
+int first_downs;
 
 int start_game();
 void drive();
@@ -10,6 +30,7 @@ void run_play();
 void defense_sim();
 
 int main() {
+    struct stats total_stats;
     char *team_name[20];
     if (start_game(team_name) == 0) {
         drive();
@@ -56,7 +77,9 @@ int start_game(char *team_name) {
 
 void drive() {
     int yard = starting_yardline();
+    int down = 0;
     
+    //while
     if (yard > 0 && yard < 50) {
         printf("\nThe ball is on your own %d-yard-line.", yard);
     } else if (yard > 50 && yard < 100) {
@@ -65,15 +88,80 @@ void drive() {
         printf("\nThe ball is on the %d-yard-line.", yard);
     }
     
-    run_play();
+    //calculate down and yards left
+    
+    run_play(yard, down);
+    
+    //new yardline and down
+    //count towards total stats
+    
+    //set of if statements before next play
 }
 
 int starting_yardline() {
     return rand() % 100 + 1;
 }
 
-void run_play() {
+void run_play(int yard, int down) {
+    int play_selection;
     
+    printf("Select a play from the following list: ");
+    
+    if (down == 1 || down == 2 || down == 3) {
+        puts("Short pass - 1, Long pass - 2, Inside run - 3, Outside run - 4");
+        printf("Play selection: ");
+        scanf("%d", &play_selection);
+        switch(play_selection) {
+            case 1:
+                //short_pass();
+            case 2:
+                //long_pass();
+            case 3:
+                //inside_run();
+            case 4:
+                //outside_run();
+            default:
+                puts("");//error message, try again
+        }
+    } else if (down == 4) {
+        if (yard < 55) {
+            printf("Short pass - 1, Long pass - 2, Inside run - 3, Outside run - 4, Punt - 5");
+            printf("Play selection: ");
+            scanf("%d", &play_selection);
+            switch(play_selection) {
+                case 1:
+                    //short_pass();
+                case 2:
+                    //long_pass();
+                case 3:
+                    //inside_run();
+                case 4:
+                    //outside_run();
+                case 5:
+                    //punt();
+                default:
+                    puts("");//error message, try again
+            }
+        } else if (yard >= 55) {
+            printf("Short pass - 1, Long pass - 2, Inside run - 3, Outside run - 4, Field goal - 5");
+            printf("Play selection: ");
+            scanf("%d", &play_selection);
+            switch(play_selection) {
+                case 1:
+                    //short_pass();
+                case 2:
+                    //long_pass();
+                case 3:
+                    //inside_run();
+                case 4:
+                    //outside_run();
+                case 5:
+                    //field_goal();
+                default:
+                    puts("");//error message, try again
+            }
+        }
+    }
 }
 
 void defense_sim() {
